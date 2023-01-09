336f455ca7c1d1b7ba7b98c8eab5ea55f100b121
Richard Link <rlink@linkdheartsranch.com>
2023-01-09 12:45:48 -0800

    building out botstate and dashboard

---
+++ src/main/java/frc/robot/subsystems/Dashboard/DashboardSubsystem.java
@@ -0,0 +1,100 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*-----------------------------------------------------------------------------\
+|                                                                              |
+|                       ================================                       |
+|                       **    TEAM 5290 - Vikotics    **                       |
+|                       ================================                       |
+|                                                                              |
+|                            °        #°                                       |
+|                            *O       °@o                                      |
+|                            O@ °o@@#° o@@                                     |
+|                           #@@@@@@@@@@@@@@                                    |
+|                           @@@@@@@@@@@@@@@                                    |
+|                           @@@@@@@@@@@@@@°                                    |
+|                             #@@@@@@@@@@@@@O....   .                          |
+|                             o@@@@@@@@@@@@@@@@@@@@@o                          |
+|                             O@@@@@@@@@@@@@@@@@@@#°                    *      |
+|                             O@@@@@@@@@@@@@@@@@@@@@#O                O@@    O |
+|                            .@@@@@@@@°@@@@@@@@@@@@@@@@#            °@@@    °@@|
+|                            #@@O°°°°  @@@@@@@@@@@@@@@@@@°          @@@#*   @@@|
+|                         .#@@@@@  o#oo@@@@@@@@@@@@@@@@@@@@@.       O@@@@@@@@@@|
+|                        o@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@°     @@@@@@@@@°|
+|                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   .@@@@@o°   |
+|          °***          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  @@@@@o     |
+|     o#@@@@@@@@@@@@.   *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@o@@@@@@      |
+|OOo°@@@@@@@@@@@@O°#@#   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       |
+|@@@@@@@@@@@@@@@@    o°  .@@@@@@@@@@@@@@@@@@@@@@@@#*@@@@@@@@@@@@@@@@@@@@       |
+|@@@@@@@@@@@@@@@*         O@@@@@@@@@@@@@@@@@@@@@@@   °@@@@@@@@@@@@@@@@@@o      |
+|@@@@#@@@@@@@@@            @@@@@@@@@@@@@@@@@@@@@@       .*@@@@@@@@@@@@@@.      |
+|@@@°      @@@@O           @@@@@@@@@@@@@@@@@@@@o           °@@@@@@@@@@@o       |
+|          @@@@@          .@@@@@@@@@@@@@@@@@@@*               O@@@@@@@*        |
+|           @@@@@        o@@@@@@@@@@@@@@@@@@@@.               #@@@@@O          |
+|           *@@@@@@@*  o@@@@@@@@@@@@@@@@@@@@@@°              o@@@@@            |
+|           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.              @@@@@#            |
+|          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@O             #@@@@@             |
+|          .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#           .@@@@@°             |
+|           @@@@@@@@@@O*    @@@@@@@@@@@@@@@@@@@@@°         °O@@@°              |
+|            °O@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@                            |
+|              o@@@@@°      @@@@@@@@@@@@@@@@@@@@@@@@                           |
+|               @@@@@@.     @@@@@@@@@@@@@@@@@@@@@@@@@o                         |
+|                @@@@@@*    @@@@@@@@@@@@@@@@@@@@@@@@@@                         |
+|                o@@@@@@.  o@@@@@@@@@@@@@@@@@@@@@@@@@@@                        |
+|                 #@@@@@@  *@@@@@@@@@@@@@@@@@@@@@@@@@@@@                       |
+|                  °***    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@O                      |
+|                         .OOOOOOOOOOOOOOOOOOOOOOOOOOOOOO                      |
+\-----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems.Dashboard;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+
+/**
+ * Dashboard subsystem that provides tabs on a Shuffleboard GUI
+ */
+public class DashboardSubsystem extends SubsystemBase {
+  /** Main driver station tab */
+  private AutoDashboard m_autoTab;
+  /** Drive train parameter tab */
+  //private DriveTrainDashboardTab m_driveTrainTab;
+
+  /** Creates a new Dashboard. */
+  public DashboardSubsystem(RobotContainer botContainer) {
+    m_autoTab = new AutoDashboard(botContainer);
+    //m_driveTrainTab = new DriveTrainDashboardTab(botContainer.driveBaseSubsystem);
+  }
+
+  public void initialize(RobotContainer botContainer) {
+    m_autoTab.initialize(botContainer);
+    //m_driveTrainTab.initialize(botContainer);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    m_autoTab.update();
+    //m_driveTrainTab.update();
+
+   // String selectedAutoName = getSelectedAutoRoutine();
+   // Trajectory trajectory = RobotContainer.pathweaverFactory.getTrajectory(selectedAutoName);
+    //setCurrentTrajectory(trajectory);
+  }
+
+  /** Returns the present selected autonomous routine 
+   * @return A String containing the name of the selected autonomous routine
+  */
+  //public String getSelectedAutoRoutine() {
+  //  return m_showtimeTab.getSelectedAutoName();
+  //}
+
+  /** Sets the current trajectory displayed by the Field2d widget */
+  //public void setCurrentTrajectory(Trajectory trajectory) {
+  //  m_showtimeTab.setCurrentTrajectory(trajectory);
+  //}
+}
