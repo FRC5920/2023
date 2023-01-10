c63ba3d985206704c92f2c898a886c83e4508790
Richard Link <rlink@linkdheartsranch.com>
2023-01-10 12:15:20 -0800

    Adding in the apriltag location file.

--- src/main/java/frc/robot/subsystems/Heimdall/PVCore.java
+++ src/main/java/frc/robot/subsystems/Heimdall/PVCore.java
@@ -2,16 +2,19 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.Heimdall;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Heimdall extends SubsystemBase {
-  /** Creates a new Heimdall. */
-  public Heimdall() {}
+public class PVCore extends SubsystemBase {
+  /** Creates a new PVCore. */
+  public PVCore() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 }
+
+
+//AprilTagFieldLayout has a constructor for loading the 2023-chargedup.json
