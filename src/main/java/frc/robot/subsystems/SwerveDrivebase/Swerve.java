c06a7146a65e43dc5a15083ce25f0ee390f780ac
Richard Link <rlink@linkdheartsranch.com>
2023-01-25 13:32:55 -0800

    fixing ambeguity with classnames.

--- src/main/java/frc/robot/subsystems/SwerveDrivebase/Swerve.java
+++ src/main/java/frc/robot/subsystems/SwerveDrivebase/Swerve.java
@@ -97,8 +97,7 @@ public class Swerve extends SubsystemBase {
                 ? ChassisSpeeds.fromFieldRelativeSpeeds(
                     translation.getX(), translation.getY(), rotation, getYaw())
                 : new ChassisSpeeds(translation.getX(), translation.getY(), rotation));
-    SwerveDriveKinematics.desaturateWheelSpeeds(
-        swerveModuleStates, Constants.SwerveDrivebaseConstants.maxSpeed);
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.maxSpeed);
 
     for (SwerveModule mod : mSwerveMods) {
       mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
