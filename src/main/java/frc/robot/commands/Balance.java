8609d4bede4e88171864ac0826a25822947a0d61
Richard Link <rlink@linkdheartsranch.com>
2023-02-02 13:20:48 -0800

    adding balance command.

---
+++ src/main/java/frc/robot/commands/Balance.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SwerveDrivebase.Swerve;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.geometry.Pose2d;
+import java.util.function.Supplier;
+
+public class Balance extends CommandBase {
+  private static final TrapezoidProfile.Constraints X_CONSTRAINTS = new TrapezoidProfile.Constraints(3, 2);
+  private static final TrapezoidProfile.Constraints Y_CONSTRAINTS = new TrapezoidProfile.Constraints(3, 2);
+  private static final TrapezoidProfile.Constraints OMEGA_CONSTRAINTS = new TrapezoidProfile.Constraints(8, 8);
+  private final ProfiledPIDController xController = new ProfiledPIDController(3, 0, 0, X_CONSTRAINTS);
+  private final ProfiledPIDController yController = new ProfiledPIDController(3, 0, 0, Y_CONSTRAINTS);
+  private final ProfiledPIDController omegaController = new ProfiledPIDController(2, 0, 0, OMEGA_CONSTRAINTS);
+
+  private final Supplier<Pose2d> poseProvider;
+  private final Swerve drivetrainSubsystem;
+
+
+  /** Creates a new Balance. */
+  public Balance(Swerve drivetrainSubsystem, Supplier<Pose2d> poseProvider) {
+    this.drivetrainSubsystem = drivetrainSubsystem;  
+    this.poseProvider = poseProvider;
+
+    xController.setTolerance(0.2);
+    yController.setTolerance(0.2);
+    omegaController.setTolerance(Units.degreesToRadians(3));
+    omegaController.enableContinuousInput(-Math.PI, Math.PI);
+
+    addRequirements(drivetrainSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    var robotPose = poseProvider.get();
+    omegaController.reset(robotPose.getRotation().getRadians());
+    xController.reset(robotPose.getX());
+    yController.reset(robotPose.getY());
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //https://www.instructables.com/Self-Balancing-Robot-Using-PID-Algorithm-STM-MC/
+//https://docs.wpilib.org/en/stable/docs/software/advanced-controls/controllers/pidcontroller.html
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drivetrainSubsystem.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
