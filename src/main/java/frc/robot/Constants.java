f6d03df33f4b08f36b8fc6857bc1253e53587119
Iron Giant <vikotics5920@gmail.com>
2023-02-19 15:10:39 -0800

    updating camera_to_robot constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -229,7 +229,7 @@ public final class Constants {
     // TODO: get the actual location of the tag camera to the robot transform
     public static final Transform3d CAMERA_TO_ROBOT =
         new Transform3d(
-            new Translation3d(0.5, 0.0, 0.5),
+          new Translation3d(0.0952, 0.0, 0.2794),
             new Rotation3d(
                 0, 0,
                 0)); // Cam mounted facing forward, half a meter forward of center, half a meter up
