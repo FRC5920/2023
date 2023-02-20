8828c57d61167959985b7fb2adafcb143a67555c
Marvin <vikotics5920@gmail.com>
2023-02-20 14:11:58 -0800

    try making fieldRelative false

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -96,7 +96,7 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    boolean fieldRelative = true;
+    boolean fieldRelative = false;
     boolean openLoop = true;
     SwerveModuleIO swerveModuleIO[];
     GyroIO gyroIO;
