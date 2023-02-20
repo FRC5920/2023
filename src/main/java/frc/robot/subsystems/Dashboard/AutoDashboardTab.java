4b6571ac24b9c9c44ee8b38ffc4346d1a8ccda38
Dave Billin <db77junk@gmail.com>
2023-02-20 15:19:35 -0800

    Add dashboard tab and constants

---
+++ src/main/java/frc/robot/subsystems/Dashboard/AutoDashboardTab.java
@@ -0,0 +1,109 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 FIRST and other WPILib contributors.
+// http://github.com/FRC5920
+// Open Source Software; you can modify and/or share it under the terms of the
+// license given in WPILib-License.md in the root directory of this project.
+////////////////////////////////////////////////////////////////////////////////
+
+/*-----------------------------------------------------------------------------\
+|                                                                              |
+|                       ================================                       |
+|                       **    TEAM 5920 - Vikotics    **                       |
+|                       ================================                       |
+|                                                                              |
+|                            °        #°                                       |
+|                            *O       °@o                                      |
+|                            O@ °o@@#° o@@                                     |
+|                           #@@@@@@@@@@@@@@                                    |
+|                           @@@@@@@@@@@@@@@                                    |
+|                           @@@@@@@@@@@@@@°                                    |
+|                             #@@@@@@@@@@@@@O....   .                          |
+|                             o@@@@@@@@@@@@@@@@@@@@@o                          |
+|                             O@@@@@@@@@@@@@@@@@@@#°                    *      |
+|                             O@@@@@@@@@@@@@@@@@@@@@#O                O@@    O |
+|                            .@@@@@@@@°@@@@@@@@@@@@@@@@#            °@@@    °@@|
+|                            #@@O°°°°  @@@@@@@@@@@@@@@@@@°          @@@#*   @@@|
+|                         .#@@@@@  o#oo@@@@@@@@@@@@@@@@@@@@@.       O@@@@@@@@@@|
+|                        o@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@°     @@@@@@@@@°|
+|                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   .@@@@@o°   |
+|          °***          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  @@@@@o     |
+|     o#@@@@@@@@@@@@.   *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@o@@@@@@      |
+|OOo°@@@@@@@@@@@@O°#@#   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       |
+|@@@@@@@@@@@@@@@@    o°  .@@@@@@@@@@@@@@@@@@@@@@@@#*@@@@@@@@@@@@@@@@@@@@       |
+|@@@@@@@@@@@@@@@*         O@@@@@@@@@@@@@@@@@@@@@@@   °@@@@@@@@@@@@@@@@@@o      |
+|@@@@#@@@@@@@@@            @@@@@@@@@@@@@@@@@@@@@@       .*@@@@@@@@@@@@@@.      |
+|@@@°      @@@@O           @@@@@@@@@@@@@@@@@@@@o           °@@@@@@@@@@@o       |
+|          @@@@@          .@@@@@@@@@@@@@@@@@@@*               O@@@@@@@*        |
+|           @@@@@        o@@@@@@@@@@@@@@@@@@@@.               #@@@@@O          |
+|           *@@@@@@@*  o@@@@@@@@@@@@@@@@@@@@@@°              o@@@@@            |
+|           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.              @@@@@#            |
+|          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@O             #@@@@@             |
+|          .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#           .@@@@@°             |
+|           @@@@@@@@@@O*    @@@@@@@@@@@@@@@@@@@@@°         °O@@@°              |
+|            °O@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@                            |
+|              o@@@@@°      @@@@@@@@@@@@@@@@@@@@@@@@                           |
+|               @@@@@@.     @@@@@@@@@@@@@@@@@@@@@@@@@o                         |
+|                @@@@@@*    @@@@@@@@@@@@@@@@@@@@@@@@@@                         |
+|                o@@@@@@.  o@@@@@@@@@@@@@@@@@@@@@@@@@@@                        |
+|                 #@@@@@@  *@@@@@@@@@@@@@@@@@@@@@@@@@@@@                       |
+|                  °***    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@O                      |
+|                         .OOOOOOOOOOOOOOOOOOOOOOOOOOOOOO                      |
+\-----------------------------------------------------------------------------*/
+package frc.robot.subsystems.Dashboard;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import frc.robot.Constants.CargoLocation;
+import frc.robot.Constants.GoalLocation;
+import frc.robot.Constants.StagingLocation;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.SwerveDrivebase.Swerve;
+import java.util.*;
+
+/** A class supplying a Shuffleboard tab for configuring drive train parameters */
+public class AutoDashboardTab implements IDashboardTab {
+
+  /** Title displayed in the dashboard tab */
+  static final String kTabTitle = "Auto Builder";
+
+  /** Width (in cells) of a swerve telemetry module on the dashboard (given a cell size of 32) */
+  static final int kSubPanelWidth = 6;
+
+  /** Height (in cells) of a swerve telemetry module on the dashboard (given a cell size of 32) */
+  static final int kSubPanelHeight = 14;
+
+  static final String kCargoNames[] = CargoLocation.getNames();
+
+  static final String kGoalNames[] = GoalLocation.getNames();
+
+  static final String kStagingLocationNames[] = StagingLocation.getNames();
+
+  /** The Shuffleboard tab */
+  private ShuffleboardTab m_tab;
+
+  /** 2d view of the field */
+  private Field2d m_field2d;
+
+  /** Creates an instance of the tab */
+  AutoDashboardTab() {
+    m_field2d = new Field2d();
+  }
+
+  /**
+   * Create and initialize dashboard widgets
+   *
+   * @param botContainer Container that holds robot subsystems
+   */
+  @Override
+  public void initDashboard(RobotContainer botContainer) {
+    m_tab = Shuffleboard.getTab(kTabTitle);
+  }
+
+  /** Service dashboard tab widgets */
+  @Override
+  public void updateDashboard(RobotContainer botContainer) {
+    Swerve swerveSubsystem = botContainer.swerveSubsystem;
+    m_field2d.setRobotPose(swerveSubsystem.getPose());
+  }
+}
