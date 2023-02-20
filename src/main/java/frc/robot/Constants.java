bb5379aafa3a9b81f052e51b1a87416a652b9172
Marvin <vikotics5920@gmail.com>
2023-02-20 15:43:02 -0800

    Adjust kGridCorrectionMultiplier

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -67,7 +67,7 @@ import org.littletonrobotics.junction.LoggedRobot;
 public final class Constants {
 
   /** multiplier for the speed in SnapToGrid command */
-  public static final double kGridCorrectionMultiplier = 1.5;
+  public static final double kGridCorrectionMultiplier = 2.0;
 
   public static final double robotPeriodSec = LoggedRobot.defaultPeriodSecs;
 
