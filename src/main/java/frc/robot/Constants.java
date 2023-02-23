27f15d5f465ff82ebd95794708cd022697861901
Iron Giant <vikotics5920@gmail.com>
2023-02-22 16:29:42 -0800

    upgrading to 2023.4.1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -232,7 +232,8 @@ public final class Constants {
             new Translation3d(0.0952, 0.0, 0.2794),
             new Rotation3d(
                 0, 0,
-                180)); // Cam mounted facing forward, half a meter forward of center, half a meter up
+                180)); // Cam mounted facing forward, half a meter forward of center, half a meter
+    // up
     // from center.
 
     public static final Transform3d ROBOT_TO_CAMERA = CAMERA_TO_ROBOT.inverse();
