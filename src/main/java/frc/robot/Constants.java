1d82900a4600858da0abd0d158793a611da11abf
Iron Giant <vikotics5920@gmail.com>
2023-02-25 10:33:58 -0800

    update to advantagekit 2.2.1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -125,7 +125,7 @@ public final class Constants {
     public static final double kFetchAngularP = 0.1;
     public static final double kFetchAngularD = 0.0;
   }
-  
+
   public static final class SwerveDrivebaseConstants {
     public static final int pigeonID = 41;
     public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
