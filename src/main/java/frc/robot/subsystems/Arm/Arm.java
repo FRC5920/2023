158f2bb9d12617c6797025fc9aadd5ff693df5eb
Marvin <vikotics5920@gmail.com>
2023-02-26 12:24:22 -0800

    import

--- src/main/java/frc/robot/subsystems/Arm/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm/Arm.java
@@ -55,8 +55,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
