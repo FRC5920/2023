17d09684952689e10b2b9dc2e03bf25b13b1cfd7
Richard Link <rlink@linkdheartsranch.com>
2023-03-12 15:50:05 -0700

    Start z-Target

---
+++ src/main/java/frc/robot/commands/zTarget.java
@@ -0,0 +1,144 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2023 FIRST and other WPILib contributors.
+// http://github.com/FRC5920
+// Open Source Software; you can modify and/or share it under the terms of the
+// license given in WPILib-License.md in the root directory of this project.
+////////////////////////////////////////////////////////////////////////////////
+
+/*-----------------------------------------------------------------------------\
+|                                                                              |
+|                       ================================                       |
+|                       **    TEAM 5920 - Vikotics    **                       |
+|                       ================================                       |
+|                                                                              |
+|                            °        #°                                       |
+|                            *O       °@o                                      |
+|                            O@ °o@@#° o@@                                     |
+|                           #@@@@@@@@@@@@@@                                    |
+|                           @@@@@@@@@@@@@@@                                    |
+|                           @@@@@@@@@@@@@@°                                    |
+|                             #@@@@@@@@@@@@@O....   .                          |
+|                             o@@@@@@@@@@@@@@@@@@@@@o                          |
+|                             O@@@@@@@@@@@@@@@@@@@#°                    *      |
+|                             O@@@@@@@@@@@@@@@@@@@@@#O                O@@    O |
+|                            .@@@@@@@@°@@@@@@@@@@@@@@@@#            °@@@    °@@|
+|                            #@@O°°°°  @@@@@@@@@@@@@@@@@@°          @@@#*   @@@|
+|                         .#@@@@@  o#oo@@@@@@@@@@@@@@@@@@@@@.       O@@@@@@@@@@|
+|                        o@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@°     @@@@@@@@@°|
+|                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   .@@@@@o°   |
+|          °***          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  @@@@@o     |
+|     o#@@@@@@@@@@@@.   *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@o@@@@@@      |
+|OOo°@@@@@@@@@@@@O°#@#   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@       |
+|@@@@@@@@@@@@@@@@    o°  .@@@@@@@@@@@@@@@@@@@@@@@@#*@@@@@@@@@@@@@@@@@@@@       |
+|@@@@@@@@@@@@@@@*         O@@@@@@@@@@@@@@@@@@@@@@@   °@@@@@@@@@@@@@@@@@@o      |
+|@@@@#@@@@@@@@@            @@@@@@@@@@@@@@@@@@@@@@       .*@@@@@@@@@@@@@@.      |
+|@@@°      @@@@O           @@@@@@@@@@@@@@@@@@@@o           °@@@@@@@@@@@o       |
+|          @@@@@          .@@@@@@@@@@@@@@@@@@@*               O@@@@@@@*        |
+|           @@@@@        o@@@@@@@@@@@@@@@@@@@@.               #@@@@@O          |
+|           *@@@@@@@*  o@@@@@@@@@@@@@@@@@@@@@@°              o@@@@@            |
+|           @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.              @@@@@#            |
+|          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@O             #@@@@@             |
+|          .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#           .@@@@@°             |
+|           @@@@@@@@@@O*    @@@@@@@@@@@@@@@@@@@@@°         °O@@@°              |
+|            °O@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@                            |
+|              o@@@@@°      @@@@@@@@@@@@@@@@@@@@@@@@                           |
+|               @@@@@@.     @@@@@@@@@@@@@@@@@@@@@@@@@o                         |
+|                @@@@@@*    @@@@@@@@@@@@@@@@@@@@@@@@@@                         |
+|                o@@@@@@.  o@@@@@@@@@@@@@@@@@@@@@@@@@@@                        |
+|                 #@@@@@@  *@@@@@@@@@@@@@@@@@@@@@@@@@@@@                       |
+|                  °***    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@O                      |
+|                         .OOOOOOOOOOOOOOOOOOOOOOOOOOOOOO                      |
+\-----------------------------------------------------------------------------*/
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.lib.Joystick.ProcessedXboxController;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.SwerveDrivebase.Swerve;
+import frc.robot.subsystems.runtimeState.BotStateSubsystem;
+import org.photonvision.PhotonCamera;
+
+public class zTarget extends CommandBase {
+  /** Creates a new Fetch. */
+  private Translation2d translation;
+
+  private boolean fieldRelative;
+  private boolean openLoop;
+
+  private Swerve s_Swerve;
+  private ProcessedXboxController controller;
+
+  PhotonCamera fetchCamera;
+  double rotation;
+  PIDController turnController =
+      new PIDController(
+          Constants.ArmConstants.kFetchAngularP, 0, Constants.ArmConstants.kFetchAngularD);
+  Arm.GamePieceType fetchTarget;
+
+  public ZTarget(
+      Arm.GamePieceType FetchWhat,
+      PhotonCamera camera,
+      Swerve s_Swerve,
+      ProcessedXboxController controller,
+      boolean fieldRelative,
+      boolean openLoop) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.s_Swerve = s_Swerve;
+    addRequirements(s_Swerve);
+
+    this.controller = controller;
+    this.fieldRelative = fieldRelative;
+    this.openLoop = openLoop;
+    fetchCamera = camera;
+    fetchTarget = FetchWhat;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    if (fetchTarget == Arm.GamePieceType.Cone) {
+      fetchCamera.setPipelineIndex(Constants.VisionConstants.kConePipelineIndex);
+    } else {
+      fetchCamera.setPipelineIndex(Constants.VisionConstants.kCubePipelineIndex);
+    }
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double yAxis = -controller.getLeftY();
+    double xAxis = -controller.getLeftX();
+
+    yAxis = (Math.abs(yAxis) < Constants.DriverConstants.stickDeadband) ? 0 : yAxis;
+    xAxis = (Math.abs(xAxis) < Constants.DriverConstants.stickDeadband) ? 0 : xAxis;
+
+    var result = fetchCamera.getLatestResult();
+
+    if (result.hasTargets()) {
+      // Calculate angular turn power
+      // -1.0 required to ensure positive PID controller effort _increases_ yaw
+      rotation = -turnController.calculate(result.getBestTarget().getYaw(), 0);
+    } else {
+      // If we have no targets, rotate according to joystick as normal.
+      rotation = -controller.getRightX();
+    }
+    translation = new Translation2d(yAxis, xAxis).times(RobotContainer.MaxSpeed);
+    rotation *= RobotContainer.MaxRotate;
+    s_Swerve.drive(translation, rotation, fieldRelative, openLoop);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    fetchCamera.setPipelineIndex(Constants.VisionConstants.kDriverCameraPipelineIndex);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
